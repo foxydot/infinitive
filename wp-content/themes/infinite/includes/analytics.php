d26f6ef98caf032de9ba651e71863736fe2a7a4d
cmos <cmos@192.168.123.29>
2011-11-04 14:43:15 -0400

    Changes to Hot Topics Blog section

--- wp-content/themes/infinite/includes/analytics.php
+++ wp-content/themes/infinite/includes/analytics.php
@@ -2,11 +2,11 @@
 function infinite_analytics_logo(){
 	if(get_blog_option(get_current_blog_id(),'blogname') == 'Analytics' && is_home()){
 		$ret = '<div class="eye-on-analytics-logo">
-		<img src="'.get_bloginfo('template_url').'/images/eyeonanalytics_sm.jpg" />
+		<img src="'.get_bloginfo('template_url').'/images/eyeonanalytics_logo.png" />
 		</div>';
 		print $ret;
 	} else {
-		$ret = '<img class="eye-on-analytics-logo logo" src="'.get_bloginfo('template_url').'/images/eyeonanalytics_sm.jpg" />';
+		$ret = '<img class="eye-on-analytics-logo logo" src="'.get_bloginfo('template_url').'/images/eyeonanalytics_logo.png" />';
 		return $ret;
 	}
 }
